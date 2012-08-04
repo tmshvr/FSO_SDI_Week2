af6ce9630d5db3c241a6114d7b3263ffb2439960
Thomas Shaver <tmshvr@Thomass-MacBook-Pro.local>
2012-08-03 21:19:07 -0500

    Added boolean expression and comments relating it to the story.

--- rename-me.js
+++ rename-me.js
@@ -1,6 +1,15 @@
 //alert("JavaScript works!");
 
 // Thomas Shaver
-// 08/02/2012
+// 08/03/2012
 // Project 2
 // ............... Description ...............
+
+var allGuests = true, decorated = true, ready = false;
+
+// If the guests are all here and we're done decorating,
+if( allGuests && decorated ) {
+// Then we're ready for Mom and Jim to come back:
+	ready = !ready;
+	console.log( "The guests are here, and the decorations are done; someone should call Jim and tell him we're ready." );
+}
