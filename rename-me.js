114bc932af04e0c9cdaad9cda3ec30a7e2fa5f19
Thomas Shaver <tmshvr@fullsail.edu>
2012-08-03 22:17:39 -0500

    Added while and for loops, and comments relating to the story.

--- rename-me.js
+++ rename-me.js
@@ -5,7 +5,23 @@
 // Project 2
 // ............... Description ...............
 
-var allGuests = true, decorated = true, ready = false;
+var allGuests = false,
+    decorated = false,
+    ready = false,
+    guests = 20,
+    arrived = 0,
+    decorations = 40,
+    decorationsUp = 0;
+
+// While we're still waiting for guests to arrive,
+while( guests < 20 ) {
+// Put up decorations
+    for( ; decorationsUp <= decorations; decorationsUp += 2 ) {
+        console.log(( decorations - decorationsUp ) + " decorations left, and " + ( guests - arrived ) + " on their way." );
+    }
+    guests++;
+}
+
 
 // If the guests are all here and we're done decorating,
 if( allGuests && decorated ) {
@@ -13,3 +29,4 @@ if( allGuests && decorated ) {
 	ready = !ready;
 	console.log( "The guests are here, and the decorations are done; someone should call Jim and tell him we're ready." );
 }
+
